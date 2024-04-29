6f6328b7185beee751f6422fbc8294e4f0fdd467
BimalDevasia <bimaldevasia@gmail.com>
2024-04-29 14:07:01 +0530

    animation added

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import "./Spinner.scss";
-import Loading from "../assets/loadingAnim.gif";
+import Loading from "../assets/giffy.gif";
+
 
 function Spinner() {
   return (
     <div className="loader-box">
       <img className="loader" src={Loading} alt="Loading . . ." />
+      
     </div>
   );
 }
